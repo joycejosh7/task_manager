7289dabfe4e3c63b66be477e51c1185b57a50d79
Joshua <joycejosh7@gmail.com>
2021-02-05 16:10:10 -0500

    added additional routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,29 @@
 Rails.application.routes.draw do
-  resources :projects
-  resources :tasks
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+    root "sessions#home"
+
+  	get '/signup' => 'users#new'
+    post '/signup' => 'users#create'
+
+    #login route
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+
+    #logout route
+    delete '/logout' => 'sessions#destroy'
+
+    #omniauth callback route
+    get "/auth/:provider/callback" => 'sessions#google'
+
+    resources :projects do
+      resources :tasks
+    end
+
+    resources :tasks
+
+    resources :users do
+      resources :projects, shallow: true
+    end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html	  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end	
+
