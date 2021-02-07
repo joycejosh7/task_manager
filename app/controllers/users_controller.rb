2d31a80492d58c9d5a669270e7cd8e12f5b6a26d
Joshua <joycejosh7@gmail.com>
2021-02-07 10:33:00 -0500

    added create action and strong params to user cont

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,23 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
-        
+      
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            #redirect to the show page
+            redirect_to @user
+        else
+            render :new
+        end
+    end
+
+    
+
+    private
+
+        def user_params
+            params.require(:user).permit(:name, :email, :password)
+        end
 end
