a66e4bd6e7d884a3edb873ba43c7283d56e71da6
Joshua <joycejosh7@gmail.com>
2021-02-07 11:09:45 -0500

    added update and private methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,5 +31,42 @@ class TasksController < ApplicationController
             end
     end
 
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @task.update(task_params)
+            redirect_to task_path(@task)
+        else  
+            render :edit
+        end
+    end
+
+    private
+
+        def task_params
+            params.require(:task).permit(
+                :name,
+                :description,
+                :due_by,
+                :project_id
+            )
+        end
+
+        def set_task
+            @task = Task.find_by(id: params[:id])
+            if !@task
+                flash[:message] = "Task was not found"
+                redirect_to tasks_path
+            end
+        end
+
+        def redirect_if_not_task_creator
+            redirect_to tasks_path if @task.user != current_user
+        end
+
 
 end
