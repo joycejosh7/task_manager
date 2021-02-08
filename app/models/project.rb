730fe65d4148aa9530e4a4f69d464a5cb9d56e78
Joshua <joycejosh7@gmail.com>
2021-02-08 11:03:28 -0500

    added associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,9 @@
 class Project < ApplicationRecord
     belongs_to :user
-    has_many :tasks, dependent: :destroy
+    has_many :tasks, dependent: :destroy 
     has_many :users, through: :tasks
 
-    validates :title, presence: true
+    validates :title, presence: true 
+    validates :description, presence: true 
+    validates :due_date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :projects
+    has_many :tasks
+
+    validates :name, presence: true 
+    validates :email, presence: true
+    validates :password, presence: true
 end
